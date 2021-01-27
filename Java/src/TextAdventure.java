f77f1fa43d6a1b03834f17bf9a29b9f08a402303
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-27 11:41:44 -0600

    first push not reading class

--- Java/src/TextAdventure.java
+++ Java/src/TextAdventure.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class TextAdventure {
+
+
+    Scanner sc = new Scanner(System.in);
+//    Ask if the user is ready to start. If they type in "yes", start the game.
+public static void main(String[] args) {
+
+    System.out.println("Do you want to start the game? :");
+    String userInput= sc.nextLine();
+
+
+
+}
+
+//    Ask the user for their name. Store this as a variable to personalize the adventure.
+
+//    A classic RPG will have the hero going through different scenarios and battling their enemies.
+
+//    Display enemy stats and hero stats. Properties and values can be assigned by you.
+
+//    For example: Health, Attack Points, etc.
+//    Decide on what actions your hero can take.
+//    Attack (decreases enemyHealth)
+//    Drink potion (adds to heroHealth)
+//    Run? (ends the game)
+//    Keep asking for user input until the enemyHealth reaches 0, then end the game.
+
+
 }
